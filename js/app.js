c68020960afb5ac4282ac5afcf30c5f007c879b3
Ashrf Mathkour Obeidat <ashrfmathkour@gmail.com>
2021-06-20 21:35:30 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,149 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <img src="https://www.kindpng.com/picc/m/161-1612585_trolley-vector-super-market-blue-shopping-cart-logo.png" alt="logo">
+        <h1>Buss Mall</h1>
+    </header>
+    <main>
+        <section id="results">
+            <button id='result-button'> view results</button>
+            <p id='resultsP'>results shown here</p>
+        </section>
+        <section id='catalog'>
+            <section id="imageSection">
+                <img id='leftImage' src="" alt="">
+                <img id='rightImage' src="" alt="">
+                <img id='middleImage' src="" alt="">
+            </section>
+            <section id="p">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis iste corrupti tempora vero natus rem, quos, inventore temporibus facilis repellat perspiciatis iusto dolore repudiandae, eius laudantium sunt architecto totam quae!</section>
+        </section>
 
-'use strict';
-
-function randomNumber( min, max ) {
-    min = Math.ceil( min );
-    max = Math.floor( max );
-    return Math.floor( Math.random() * ( max - min + 1 ) + min );
-}
-
-let imgArray=[
-    'bag.jpg','banana.jpg','bathroom.jpg','boots.jpg',
-    'breakfast.jpg','bubblegum.jpg','chair.jpg','cthulhu.jpg','dog-duck.jpg','dragon.jpg',
-    'pen.jpg','pet-sweep.jpg','scissors.jpg','shark.jpg','sweep.png','tauntaun.jpg','unicorn.jpg',
-    'usb.gif','water-can.jpg','wine-glass.jpg']
-let imageSection=document.getElementById('imageSection')
-let leftImage=document.getElementById('leftImage');
-let middleImage=document.getElementById('middleImage');
-let rightImage=document.getElementById('rightImage');
-let resultsP=document.getElementById('resultsP');
-let resultButton=document.getElementById('result-button');
-let counter=0;
-let results=[];
-
-
-
-function Item(name,src,view){
-    this.name=name;
-    this.src=`img/${src}`;
-    this.view=0;
-    this.clicked=0;
-    Item.product.push(this);
-}
-
-
-Item.product = []
-console.log(Item.product)
-
-for( let i = 0; i < imgArray.length; i++ ) {
-
-new Item(imgArray[i].split('.')[0],imgArray[i])
-
-}
-
-
-
-
-
-
-
- function render() {
-    let leftIndex = randomNumber(0, imgArray.length - 1);
-    let middleIndex;
-    let rightIndex;
-  
-    do {
-      rightIndex = randomNumber(0, imgArray.length - 1);
-       middleIndex=randomNumber(0, imgArray.length - 1);
-    } while( leftIndex === rightIndex ||leftIndex === middleIndex
-       ||middleIndex === rightIndex );
-
-      //  console.log(Item.product[middleIndex].src)
-
-       leftImage.src=Item.product[leftIndex].src;
-       middleImage.src=Item.product[middleIndex].src;
-       rightImage.src=Item.product[rightIndex].src;
-     
-
-       ///views counter
-       Item.product[rightIndex].view++;
-       Item.product[middleIndex].view++;
-       Item.product[middleIndex].view++;
-      
-    
-       
-
-     
-
-     
-     
-}
-
-
-function eventHandler(e) {
-    // console.log(e.target.id);
-    if((e.target.id === 'rightImage' || e.target.id === 'leftImage'||event.target.id==='middleImage') && counter < 25){
-      render();
-      
-      counter++;
-  
-    } else if (counter===25){
-      alert('press on the button to see the results')
-      resultButton.addEventListener('click',result)
-    
-     
-     
-
-
-      
-      
-        
-      
-    }
-    
-  
-  }
-imageSection.addEventListener('click',eventHandler);
-
-
-
-
-
-render();
-
-///// clicked counter 
-leftImage.addEventListener('click',somthing)
-middleImage.addEventListener('click',somthing)
-rightImage.addEventListener('click',somthing)
-
-function somthing(e){
-  for(let j=0;j<imgArray.length;j++){
-
- if (e.target.src.split('00/')[1]===Item.product[j].src){
-   Item.product[j].clicked++
-   console.log(Item.product[j].clicked)
-      
- }
- 
-  }
-
-}
-
-
-function result(){
-  
-  if(counter===25){
-    for( let i = 0; i < imgArray.length; i++ ) {
-    //  console.log(`${Item.product[i].name} had ${Item.product[i].clicked} votes
-    //  and was viewed ${Item.product[i].view} times `)
-      console.log(Item.product[j].clicked)
-     results.push(`${Item.product[i].name} had ${Item.product[i].clicked} votes
-     and was viewed ${Item.product[i].view} times `)
-
-  }
-  resultsP.textContent=results;
-  
-}
-  }
-
-
+    </main>
+    <footer>
+        <p>Footer</p>
+    </footer>
+    <script src="js/app.js"></script>
+</body>
+</html>
