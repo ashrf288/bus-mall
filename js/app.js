021463b611e65a7ec563eea1ca5f427b1396be0b
Ashrf Mathkour Obeidat <ashrfmathkour@gmail.com>
2021-06-21 15:15:23 +0300

    aa

--- js/app.js
+++ js/app.js
@@ -29,9 +29,12 @@ function Item(name,src,view){
     this.view=0;
     this.clicked=0;
     Item.product.push(this);
+    Item.nameS.push(name)
+   
 }
-
-
+Item.totalViews=[]
+Item.totalClicks=[]
+Item.nameS=[]
 Item.product = []
 console.log(Item.product)
 
@@ -46,18 +49,54 @@ new Item(imgArray[i].split('.')[0],imgArray[i])
 
 
 
+let oldLeftIndex=0;
+    let oldMiddleIndex=0;
+    let oldRightIndex=0;
+
 
  function render() {
     let leftIndex = randomNumber(0, imgArray.length - 1);
     let middleIndex;
     let rightIndex;
-  
+   
     do {
       rightIndex = randomNumber(0, imgArray.length - 1);
        middleIndex=randomNumber(0, imgArray.length - 1);
     } while( leftIndex === rightIndex ||leftIndex === middleIndex
        ||middleIndex === rightIndex );
 
+      if (leftIndex===oldLeftIndex ||leftIndex === oldMiddleIndex
+        ||leftIndex === oldRightIndex||rightIndex===oldLeftIndex ||rightIndex === oldMiddleIndex
+        ||rightIndex === oldRightIndex||middleIndex===oldLeftIndex ||middleIndex === oldMiddleIndex
+        ||middleIndex === oldRightIndex){
+          console.log(' need')
+          do {
+            leftIndex= randomNumber(0, imgArray.length - 1);
+            rightIndex = randomNumber(0, imgArray.length - 1);
+             middleIndex=randomNumber(0, imgArray.length - 1);
+          } while( leftIndex===oldLeftIndex ||leftIndex === oldMiddleIndex
+            ||leftIndex === oldRightIndex||rightIndex===oldLeftIndex ||rightIndex === oldMiddleIndex
+            ||rightIndex === oldRightIndex||middleIndex===oldLeftIndex ||middleIndex === oldMiddleIndex
+            ||middleIndex === oldRightIndex ||leftIndex === rightIndex ||leftIndex === middleIndex
+            ||middleIndex === rightIndex);
+        }else{
+          console.log('no need')
+        }
+
+       console.log(leftIndex)
+       console.log(oldLeftIndex)
+        oldLeftIndex=leftIndex;
+        oldMiddleIndex=middleIndex;
+        oldRightIndex=rightIndex;
+
+
+
+
+        
+
+        
+
+        
       //  console.log(Item.product[middleIndex].src)
 
        leftImage.src=Item.product[leftIndex].src;
@@ -69,12 +108,6 @@ new Item(imgArray[i].split('.')[0],imgArray[i])
        Item.product[rightIndex].view++;
        Item.product[middleIndex].view++;
        Item.product[middleIndex].view++;
-      
-    
-       
-
-     
-
      
      
 }
@@ -121,7 +154,7 @@ function somthing(e){
  if (e.target.src.split('00/')[1]==Item.product[j].src){
      
    Item.product[j].clicked++
-   console.log(Item.product[j].clicked)
+  //  console.log(Item.product[j].clicked)
  }
  
   }
@@ -138,11 +171,77 @@ function result(){
 
      results.push(`${Item.product[i].name} had ${Item.product[i].clicked} votes
      and was viewed ${Item.product[i].view} times `)
-
+      Item.totalViews.push(Item.product[i].view);
+      Item.totalClicks.push(Item.product[i].clicked);
   }
   resultsP.textContent=results;
+
   
 }
+chart();
   }
 
   
+
+
+
+  function chart(){
+    
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: Item.nameS,
+                datasets: [{
+                    label: '# of Viwes',
+                    data: Item.totalViews,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                },{
+                  label: '# of Viwes',
+                  data: Item.totalClicks,
+                  backgroundColor: [
+                      'rgba(255, 99, 132, 0.2)',
+                      'rgba(54, 162, 235, 0.2)',
+                      'rgba(255, 206, 86, 0.2)',
+                      'rgba(75, 192, 192, 0.2)',
+                      'rgba(153, 102, 255, 0.2)',
+                      'rgba(255, 159, 64, 0.2)'
+                  ],
+                  borderColor: [
+                      'rgba(255, 99, 132, 1)',
+                      'rgba(54, 162, 235, 1)',
+                      'rgba(255, 206, 86, 1)',
+                      'rgba(75, 192, 192, 1)',
+                      'rgba(153, 102, 255, 1)',
+                      'rgba(255, 159, 64, 1)'
+                  ],
+                  borderWidth: 1
+              }
+              ]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+        
+  }
